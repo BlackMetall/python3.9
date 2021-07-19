4b0477027fc34da575623508af51f6de68eed0f2
BlackMetall <blackmetalrull@gmail.com>
2021-07-19 23:15:41 +0300

    Add to variable etajey and kvartir

--- Curier.py
+++ Curier.py
@@ -1,9 +1,11 @@
 kvart = int(input('Введите квартиру для курьера: '))
+etajey = int(input('Введите колличество этажей в подьезде: '))
+n = int(input("Введите колличество квартир на этаже: "))
 def curier(kvart,etajey,n):
     np = int(1 + (kvart - 1) / (n*etajey))
     ne = int(1 + ((kvart - 1) % (n*etajey)) / n)
     print("Нужная квартира для курьера находится в: " + str(np) + ' подъездe' + ' на ' + str(ne)+ ' этаже')
-curier(kvart,5,4)
+curier(kvart,etajey,n)
 
 
 
